3d4977f943e7911455706c914b6c2999899eda31
Даниил Якубов <95945846+REIGEN06@users.noreply.github.com>
2022-06-29 13:07:06 +0300

    Связующие таблицы у месторождения

--- app/Imports/field_Import.php
+++ app/Imports/field_Import.php
@@ -19,43 +19,42 @@ class field_Import implements ToCollection, WithHeadingRow
     */
     public function collection(Collection $rows)
     {
-        foreach ($rows as $row) {
-            $field = field::firstOrCreate([
-            'name' => rtrim(explode('(',$row['mestorozdenie_deistvuiushhie_licenzii'])[0]),
-            'degree_of_development'=> $row['stepen_osvoeniia'],
-            // 'polygon' => $row['geom_geometrymultipolygon'],
-        ]);
-        }
+        // foreach ($rows as $row) {
+        //     $field = field::firstOrCreate([
+        //     'name' => rtrim(explode('(',$row['mestorozdenie_deistvuiushhie_licenzii'])[0]),
+        //     'degree_of_development'=> $row['stepen_osvoeniia'],
+        //     // 'polygon' => $row['geom_geometrymultipolygon'],
+        // ]);
+        // }
 
             
-        foreach ($rows as $row) {
-            // dd($row);
-            $field_id = DB::table('field')
-            ->where('name', rtrim(explode('(',$row['mestorozdenie_deistvuiushhie_licenzii'])[0]))
-            ->value('id');
+        // foreach ($rows as $row) {
+        //     // dd($row);
+        //     $field_id = DB::table('field')
+        //     ->where('name', rtrim(explode('(',$row['mestorozdenie_deistvuiushhie_licenzii'])[0]))
+        //     ->value('id');
 
-            $subject_id = DB::table('subject_rf')
-            ->where('shortname', $row['subieekt_rf'])
-            ->value('id');
+        //     $subject_id = DB::table('subject_rf')
+        //     ->where('shortname', $row['subieekt_rf'])
+        //     ->value('id');
 
-            $field_subject_rf = field_subject_rf::firstOrCreate([
-            'field_id' => $field_id,
-            'subject_rf_id' => $subject_id,
-        ]);
-        }
-
-        set_time_limit(360);
+        //     $field_subject_rf = field_subject_rf::firstOrCreate([
+        //     'field_id' => $field_id,
+        //     'subject_rf_id' => $subject_id,
+        // ]);
+        // }
+            set_time_limit(360);
         foreach ($rows as $row) {
             $field_id = DB::table('field')
             ->where('name', rtrim(explode('(',$row['mestorozdenie_deistvuiushhie_licenzii'])[0]))
             ->value('id');
             
             foreach(preg_split('/\r\n|\r|\n/', $row['licenzionnye_ucastki']) as $licence_area) {
-                // dd($licence_area);
-                if($licence_area = '') break;
-                // dd($licence_area);
-                $licence_area = rtrim('(', $licence_area)[0];
-                // dd($licence_area);
+                
+                if($licence_area == '') break; 
+                
+                $licence_area = rtrim(explode('(', $licence_area)[0]);
+                
                 $licence_area_id = DB::table('licence_area')
                 ->where('name', $licence_area)
                 ->value('id');
