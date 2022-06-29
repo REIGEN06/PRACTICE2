d665371fb9a9270a161f42745d3ac9d1907196f1
Даниил Якубов <95945846+REIGEN06@users.noreply.github.com>
2022-06-29 12:10:04 +0300

    Добавление предыдущей лицензии

--- app/Imports/licence_Import.php
+++ app/Imports/licence_Import.php
@@ -24,49 +24,53 @@ class licence_Import implements ToCollection, WithHeadingRow
     */
     public function collection(Collection $rows)
     {
-        foreach ($rows as $row) {
+        // foreach ($rows as $row) {
 
-        $licence_author = licence_author::firstOrCreate([
-            'name' =>$row['organ_vydavsii_licenziiu']
-        ]);
+        // $licence_author = licence_author::firstOrCreate([
+        //     'name' =>$row['organ_vydavsii_licenziiu']
+        // ]);
 
-        $licence_status = licence_status::firstOrCreate([
-            'status' =>$row['status_licenzii']
-        ]);
+        // $licence_status = licence_status::firstOrCreate([
+        //     'status' =>$row['status_licenzii']
+        // ]);
 
-        $licence_condition = licence_condition::firstOrCreate([
-            'condition' =>$row['sostoianie_licenzii']
-        ]);
+        // $licence_condition = licence_condition::firstOrCreate([
+        //     'condition' =>$row['sostoianie_licenzii']
+        // ]);
 
-        $type_of_main_mineral = type_of_main_mineral::firstOrCreate([
-            'name' =>$row['vid_osnovnogo_poleznogo_iskopaemo']
-        ]);
+        // $type_of_main_mineral = type_of_main_mineral::firstOrCreate([
+        //     'name' =>$row['vid_osnovnogo_poleznogo_iskopaemo']
+        // ]);
 
-        $licence_area = licence_area::firstOrCreate([
-            'name' =>$row['licenzionnyi_ucastok']
-        ]);
+        // $licence_area = licence_area::firstOrCreate([
+        //     'name' =>$row['licenzionnyi_ucastok']
+        // ]);
 
-        $licence = licence::firstOrCreate([
-                'series' => $row["seriia"],
-                'number'=> $row["nomer"],
-                'view'=> $row["vid"],
-                'date_of_receiving'=> Date::excelToDateTimeObject($row["data_registracii"])->format('Y-m-d'),
-                'date_of_expiration'=> Date::excelToDateTimeObject($row["data_okoncaniia"])->format('Y-m-d'),
-                'date_of_annulment'=> Date::excelToDateTimeObject($row["data_annulirovaniia"])->format('Y-m-d'),
-                'polygon'=> $row["geom_geometrymultipolygon"],
-                'subsoil_user_id' => DB::table('subsoil_user')->where('name', $row['nedropolzovatel'])->value('id'),
-                'licence_area_id' => DB::table('licence_area')->where('name', $row['licenzionnyi_ucastok'])->value('id'),
-                'type_of_main_mineral_id' => DB::table('type_of_main_mineral')->where('name', $row['vid_osnovnogo_poleznogo_iskopaemo'])->value('id'),
-                'licence_condition_id' => DB::table('licence_condition')->where('condition', $row['sostoianie_licenzii'])->value('id'),
-                'licence_status_id' => DB::table('licence_status')->where('status', $row['status_licenzii'])->value('id'),
-                'licence_author_id' => DB::table('licence_author')->where('name', $row['organ_vydavsii_licenziiu'])->value('id'),
-        ]);
-        }
+        // $licence = licence::firstOrCreate([
+        //         'series' => $row["seriia"],
+        //         'number'=> $row["nomer"],
+        //         'view'=> $row["vid"],
+        //         'date_of_receiving'=> Date::excelToDateTimeObject($row["data_registracii"])->format('Y-m-d'),
+        //         'date_of_expiration'=> Date::excelToDateTimeObject($row["data_okoncaniia"])->format('Y-m-d'),
+        //         'date_of_annulment'=> Date::excelToDateTimeObject($row["data_annulirovaniia"])->format('Y-m-d'),
+        //         'polygon'=> $row["geom_geometrymultipolygon"],
+        //         'subsoil_user_id' => DB::table('subsoil_user')->where('name', $row['nedropolzovatel'])->value('id'),
+        //         'licence_area_id' => DB::table('licence_area')->where('name', $row['licenzionnyi_ucastok'])->value('id'),
+        //         'type_of_main_mineral_id' => DB::table('type_of_main_mineral')->where('name', $row['vid_osnovnogo_poleznogo_iskopaemo'])->value('id'),
+        //         'licence_condition_id' => DB::table('licence_condition')->where('condition', $row['sostoianie_licenzii'])->value('id'),
+        //         'licence_status_id' => DB::table('licence_status')->where('status', $row['status_licenzii'])->value('id'),
+        //         'licence_author_id' => DB::table('licence_author')->where('name', $row['organ_vydavsii_licenziiu'])->value('id'),
+        // ]);
+        // }
 
         foreach ($rows as $row) {
                 $previous_licence_id = DB::table('licence')
-                ->where(DB::raw("CONCAT('series', 'number', 'view')"), $row['predydushhaia_licenziia'])
-                ->value('id');
+                ->select('id')
+                ->where(DB::raw('CONCAT(series, number, view)'), '=', $row['predydushhaia_licenziia'])
+                ->get();
+
+                if (count($previous_licence_id) == 0) continue;
+                $previous_licence_id = $previous_licence_id[0]->id;
 
                 $previous_licence = licence::where('series', $row["seriia"])
                 ->where('number', $row["nomer"])
